846a37673afbf94f4ff21a79f0eb6b54bd634e24
pke204 <pke204@exeter.ac.uk>
2025-09-30 15:51:03 +0000

    Singular 'bottle' for final pass

--- bottles.py
+++ bottles.py
@@ -1,11 +1,17 @@
-bottles = 0
+bottles = 10
 
-while bottles < 10:
+while bottles > 1:
     print(bottles, "green bottles hanging on the wall,")
     print(bottles, "green bottles hanging on the wall,")
     print("and if one green bottle should accidentally fall,")
     print("they'll be", bottles-1, "green bottles hanging on the wall.")
     print()
-    bottles = bottles + 3
+    bottles = bottles -1
+
+print(bottles, "green bottle hanging on the wall,")
+print(bottles, "green bottle hanging on the wall,")
+print("and if one green bottle should accidentally fall,")
+print("they'll be no green bottles hanging on the wall.")
+print()
 
 print('All gone')
